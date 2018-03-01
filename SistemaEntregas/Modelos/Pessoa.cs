3c7faa4210c6870070e51537fa08d4fb3dce2b5c
alvarocintra <alvarocintra21@gmail.com>
2018-02-28 21:57:32 -0300

    Adicionando a classe Pessoa

---
+++ SistemaEntregas/Modelos/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace Modelos
+{
+    class Pessoa
+    {
+        public int PessoaID { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+
+    }
+}
