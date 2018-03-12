99ce1f05d751a6558924d45381235e21c324db0a
alvarocintra <alvarocintra21@gmail.com>
2018-03-12 20:29:27 -0300

    Criando pesquisa por nome do cliente

--- SistemaEntregas/Controller/ClienteController.cs
+++ SistemaEntregas/Controller/ClienteController.cs
@@ -1,5 +1,6 @@
 ﻿using Modelos; //usando modelos
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Controller
 {
@@ -11,6 +12,17 @@ namespace Controller
         {
             // TODO: Persistir os dados do cliente
             MeusClientes.Add(cliente);
+        }
+
+        public Cliente PesquisarPorNome(string nome)
+        {
+            var c = from x in MeusClientes
+                    where x.Nome.Equals(nome)
+                    select x;
+            if (c != null)
+                return c.FirstOrDefault();
+            else
+                return null;
         }
     }
 }
